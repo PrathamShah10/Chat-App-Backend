f6623dcfc4e350f492e9d06240a507092241524a
Pratham Shah <prathamsanjayshah2001@gmail.com>
2022-12-26 15:47:00 +0530

    cors origin

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const port = process.env.PORT || 8900;
 
 const io = require("socket.io")(port, {
     cors: {
-        origin: "https://buzztalker122.onrender.com",
+        origin: "*",
     },
 });
 
